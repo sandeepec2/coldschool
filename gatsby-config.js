de61062a960349f3a884447e88e512544d2d1b6f
Sandeep <sandeep@edunuts.com>
2020-11-29 15:24:42 +0530

    meta changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Cold School - Coming Soon',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Coming soon as Cold School',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
